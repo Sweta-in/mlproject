655f89523ce1020274be074c63409400a8b64f35
Sweta-in <swetajha1123@gmail.com>
2025-06-10 20:33:34 +0530

    Deployment configuration

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,12 +1,12 @@
 import sys
 from dataclasses import dataclass
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
@@ -16,19 +16,17 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    def __init__(self):
-        self.preprocessor_obj_file_path=os.path.join('artifacts', "preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-
         '''
-        This function is responsible for data transformation
+        This function si responsible for data trnasformation
+        
         '''
-
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
@@ -37,13 +35,12 @@ class DataTransformation:
                 "parental_level_of_education",
                 "lunch",
                 "test_preparation_course",
-
             ]
 
             num_pipeline= Pipeline(
                 steps=[
-                    ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("scaler", StandardScaler(with_mean=False))
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
 
                 ]
             )
@@ -51,33 +48,32 @@ class DataTransformation:
             cat_pipeline=Pipeline(
 
                 steps=[
-                    ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder()),
-                    ("scaler",StandardScaler(with_mean=False))
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
                 ]
+
             )
 
-
-            logging.info("Numerical columns standard scaling completed: {categorical_columns}")
-
-            logging.info("Categorical columns encoding completed: {numerical_columns}")
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
 
             preprocessor=ColumnTransformer(
                 [
-                    ("num_pipeline",num_pipeline,numerical_columns),
-                    ("cat_pipelines",cat_pipeline,categorical_columns)
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
                 ]
+
+
             )
 
             return preprocessor
-
-
-            
+        
         except Exception as e:
-            raise CustomException(e, sys)
-            
-
-    def initiate_data_transformation(self, train_path,test_path):
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
 
         try:
             train_df=pd.read_csv(train_path)
@@ -92,15 +88,14 @@ class DataTransformation:
             target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df = train_df[target_column_name]
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df = test_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
-            
             )
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
@@ -109,24 +104,21 @@ class DataTransformation:
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
-            test_arr = np.c_[
-                input_feature_test_arr, np.array(target_feature_test_df)
-            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
 
             save_object(
-                
+
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
+
             )
 
-            return ( 
+            return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
-
         except Exception as e:
             raise CustomException(e,sys)
-            
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,37 +17,35 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object, evaluate_models
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts", "model.pkl")
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
-            X_train, y_train, X_test, y_test=(
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -69,11 +67,6 @@ class ModelTrainer:
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
-                "K-Neighbors Regressor":{
-                    'n_neighbors':[5,7,9,11],
-                    # 'weights':['uniform', 'distance'],
-                    # 'algorithm':['ball_tree', 'kd_tree', 'brute']
-                },
                 "XGBRegressor":{
                     'learning_rate':[.1,.01,.05,.001],
                     'n_estimators': [8,16,32,64,128,256]
@@ -91,10 +84,10 @@ class ModelTrainer:
                 
             }
 
-
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train,X_test=X_test,y_test= y_test, models=models, param=params)
-
-            ## To get the best model score from dict
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
@@ -102,7 +95,6 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
@@ -112,18 +104,20 @@ class ModelTrainer:
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
-
             )
 
-            predicted=best_model.predict(X_test)
+            predicted=best_model.predict(X_test)[0]
+            predicted=max(0,min(100, predicted)) # Ensure predictions are within a reasonable range
+            # logging.info(f"Best model found on both training and testing dataset with r2 score: {best_model_score}")
+            # logging.info(f"Best model found on both training and testing dataset with name: {best_model_name}")
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
-
-
-
-        except Exception as e:
-            raise CustomException(e, sys)
             
-        
+
+
+
+            
+        except Exception as e:
+            raise CustomException(e,sys)
     
